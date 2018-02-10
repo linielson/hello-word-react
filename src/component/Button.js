0305f06d6b4fa6428d0ed7ba79bfe410ec46f53b
Linielson Rosa <linielsonrosa@gmail.com>
2018-02-10 14:38:43 -0200

    Add event click at Button component

--- src/component/Button.js
+++ src/component/Button.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
 
 class Button extends React.Component {
+  onClick = () => {
+    console.log(`Clicked at ${this.props.symbol}`)
+  }
+
   render() {
-    return <button>{this.props.symbol}</button>;
+    return (
+      <button
+        onClick={ this.onClick }
+      >
+        { this.props.symbol }
+      </button>
+    );
   }
 }
 
