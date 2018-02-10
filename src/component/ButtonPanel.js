e2509fea51f540582856f559e67c694e3c645d5d
Linielson Rosa <linielsonrosa@gmail.com>
2018-02-10 14:17:37 -0200

    Add calculator keys

--- src/component/ButtonPanel.js
+++ src/component/ButtonPanel.js
@@ -3,7 +3,39 @@ import Button from './Button';
 
 class ButtonPanel extends React.Component {
   render() {
-    return <Button symbol='+'/>;
+    return (
+      <div>
+        <div>
+          <Button symbol='AC'/>
+          <Button symbol='+/-'/>
+          <Button symbol='%'/>
+          <Button symbol='/'/>
+        </div>
+        <div>
+          <Button symbol='7'/>
+          <Button symbol='8'/>
+          <Button symbol='9'/>
+          <Button symbol='x'/>
+        </div>
+        <div>
+          <Button symbol='4'/>
+          <Button symbol='5'/>
+          <Button symbol='6'/>
+          <Button symbol='-'/>
+        </div>
+        <div>
+          <Button symbol='1'/>
+          <Button symbol='2'/>
+          <Button symbol='3'/>
+          <Button symbol='+'/>
+        </div>
+        <div>
+          <Button symbol='0'/>
+          <Button symbol=','/>
+          <Button symbol='='/>
+        </div>
+      </div>
+    );
   }
 }
 
