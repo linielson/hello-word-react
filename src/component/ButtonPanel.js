dee765c3c874121e43f87fef5b92e25c60bc7e72
Linielson Rosa <linielsonrosa@gmail.com>
2018-02-10 14:01:03 -0200

    Create new components

---
+++ src/component/ButtonPanel.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class ButtonPanel extends React.Component {
+  render() {
+    return <div>Awesome button panel</div>;
+  }
+}
+
+export default ButtonPanel;
---
+++ src/component/Display.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Display extends React.Component {
+  render() {
+    return (
+      <div>
+        Awesome Display
+      </div>
+    );
+  }
+}
+
+export default Display;
